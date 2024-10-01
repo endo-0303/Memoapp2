37d6bd05e43fb5ed82cc70e9afccb32697fc6622
endo-0303 <endo024932@gmail.com>
2024-10-01 12:33:36 +0900

    @expo/cliの導入

---
+++ src/components/Hello.tsx
@@ -0,0 +1,11 @@
+import { View , Text , StyleSheet } from "react-native";
+
+const Hello = (): JSX.Element => {
+  return(
+    <View>
+      <Text>Hello</Text>
+      </View>
+  )
+}
+
+export default Hello
